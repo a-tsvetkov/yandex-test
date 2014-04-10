ce6bba462b226d911bdb9389d6a9946f1d3adfc0
Alexander Tsvetkov <sickuenser@gmail.com>
2014-04-10 23:26:38 +0400

    Moved to Cython

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
 
 
 setup(
     name='Logalyzer',
     version='0.1',
     description='Log analyzer for yandex job interview',
+    install_requires=['cython'],
     author='Alex Tsvetkov',
     author_email='sickuenser@gmail.com',
     package_dir={'': 'src'},
@@ -14,5 +17,12 @@ setup(
         'console_scripts': [
             'logalyzer = logalyzer.run:main'
         ]
-    }
+    },
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension('logalyzer.analyzer', ["src/logalyzer/analyzer.pyx"]),
+        Extension('logalyzer.requests', ["src/logalyzer/requests.pyx"]),
+        Extension('logalyzer.stats', ["src/logalyzer/stats.pyx"]),
+        Extension('logalyzer.utils', ["src/logalyzer/utils.pyx"])
+    ]
 )
